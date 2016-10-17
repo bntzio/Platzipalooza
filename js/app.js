06fe4d4748585154173a2449a92ac1493051b9bd
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-10-17 13:47:33 -0500

    off-canvas menu

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ $('[data-toggle-dia]').click(function(ev) {
   var panel = $(this).data('toggleDia');
   $('#lineup-tabs').foundation('selectTab', panel);
 });
+
+var $offCanvas = $('#offCanvas');
+var $sticky = $('#sticky');
+
+$offCanvas.find('li').click(function(ev) {
+  $offCanvas.foundation('close');
+  setTimeout(function() {
+    $sticky.css('left', '0px');
+  }, 300);
+});
